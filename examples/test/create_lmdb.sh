d55dd7b4bd25c44aa82451532df94c539c531c43
alfredxiangwu <alfredxiangwu@gmail.com>
2015-01-06 14:57:12 +0800

    hybrid init

---
+++ examples/test/create_lmdb.sh
@@ -0,0 +1,5 @@
+
+rm -rf ../../examples/test/MBGC_lmdb
+Glog_logtostderr=0 Glog_alsogtostderr=1 ../../.build_release/tools/convert_features.bin ../../examples/test/ ../../data/ ../../data/list.txt ../../examples/test/MBGC_lmdb 1
+
+
